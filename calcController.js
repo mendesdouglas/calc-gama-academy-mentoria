fb12d3d0f6c51e955027648d61d9b2af449f13e0
Douglas Mendes <lixao_9@hotmail.com>
2022-08-21 08:59:10 -0300

    feat: improving switch case to recognize all numbers and operators and some methods: addOperation that will sum numbers and concat elements operators

--- calcController.js
+++ calcController.js
@@ -1,5 +1,6 @@
 class CalcController{
     constructor(){
+        this._expressions = []
         this._displayCalcElement = document.querySelector('.main-display')
         this._subDisplayCalcElement = document.querySelector('.sub-display')
         
@@ -14,14 +15,121 @@ class CalcController{
         this._subDisplayCalcElement.innerHTML='50x20'
     }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        })
+    }
+
+    clearAll(){
+        this._expressions = []
+    }
+
+    
+
+
+    setError(){
+        this.displayCalc = "ERROR"
+    }
+
+    addOperation(value){
+        this._expressions.push(value)
+        console.log(this._expressions)
+    }
+
+    execButton(value){
+        switch(value){
+            case 'C grow':
+                this.clearAll();
+                break
+
+            case 'percent':
+                console.log('cliquei na percent')
+                break
+            
+            case 'slash':
+                console.log('cliquei na /')
+                break
+
+            case 'arrow':
+                console.log('cliquei na backspace')
+                break
+                
+            case 'ast':
+                console.log('cliquei na *')
+                break
+            
+            case 'dash':
+                console.log('cliquei na -')
+                break
+
+            case 'plus':
+                console.log('cliquei na plus')
+                break
+
+            case 'equal':
+                console.log('cliquei na equal')
+                break
+
+            case 'dot':
+                console.log('cliquei na dot')
+                              
+
+            case '0 zero':
+                console.log('cliquei na 0')
+            
+            case '1':
+                console.log('cliquei na 1')
+                
+
+            case '2':
+                console.log('cliquei na 2')
+                
+
+            case '3':
+                console.log('cliquei na 3')
+
+            case '4':
+                console.log('cliquei na 4')
+                
+
+            case '5':
+                console.log('cliquei na 5')
+                
+
+            case '6':
+                console.log('cliquei na 6')
+
+            case '7':
+                console.log('cliquei na 7')
+                
+
+            case '8':
+                console.log('cliquei na 8')
+                
+
+            case '9':
+                this.addOperation(parseInt(value))
+                break
+        
+            default:
+                this.setError();
+                break;
+    
+        }
+    }
+
     initButtonsEvents(){
         let buttonsElements = document.querySelectorAll(".buttons > div")
         
         buttonsElements.forEach((btn, index)=> {
-            btn.addEventListener('click', (e) => {
+            this.addEventListenerAll(btn, 'click drag', (e) => {
                 console.log(btn.className.replace('button btn-', ''))
+                let buttonsText = btn.className.replace('button btn-', '')
+                this.execButton(buttonsText);
 
             })
+
         })
 
     }
