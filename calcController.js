6396d00297c343a6cb8bd040e0e90ad63f7db50c
Douglas Mendes <lixao_9@hotmail.com>
2022-08-21 23:11:09 -0300

    fix: fixing problem with = and when the user click more times in = button the system will sum

--- calcController.js
+++ calcController.js
@@ -1,5 +1,7 @@
 class CalcController{
     constructor(){
+        this._lastOperator = ''
+        this._lastNumber = ''
         this._operation = []
         this._displayCalcElement = document.querySelector('.main-display')
         this._subDisplayCalcElement = document.querySelector('.sub-display')
@@ -80,11 +82,39 @@ class CalcController{
         this.subDisplayCalc = this._operation.join("")
     }
 
+    getResult(){
+        return eval(this._operation.join(""))
+    }
+
     calc(){
+        let last = '';
+
+        this._lastOperator = this.getLastItem();
+
+        if (this._operation.length < 3) {
+
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
+
+        }
+
+        if (this._operation.length > 3) {
+
+            last = this._operation.pop();
+            this._lastNumber = this.getResult();
+
+        } else if (this._operation.length === 3) {
+
+            this._lastNumber = this.getLastItem(false);
+
+        }
+
+        console.log('_lastOperator', this._lastOperator)
+        console.log('_lastNumber', this._lastNumber)
+
+
+        let result = this.getResult()
         
-        let last = this._operation.pop()
- 
-        let result = eval(this._operation.join(""))
         
         if(last=== '%'){
 
@@ -106,16 +136,32 @@ class CalcController{
 
     }
 
-    setLastNumberToDisplay(){
+    getLastItem(isOperator = true) {
 
-        let lastNumber 
+        let lastItem;
 
-        for(let i = this._operation.length-1; i>=0;i--){
-            if(!this.isOperator(this._operation[i])){
-                lastNumber = this._operation[i]
-                break
+        for (let i = this._operation.length - 1; i >= 0; i--) {
+
+            if (this.isOperator(this._operation[i]) === isOperator) {
+                lastItem = this._operation[i];
+                break;
             }
+
         }
+
+        if (!lastItem) {
+
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+
+        }
+
+        return lastItem;
+
+    }
+
+    setLastNumberToDisplay(){
+        let lastNumber = this.getLastItem(false)
+        
         this.displayCalc = lastNumber
         this.subDisplayCalc = this._operation.join("")
     }
@@ -184,7 +230,7 @@ class CalcController{
                 break
 
             case 'equal':
-                this.calcFromEqual()
+                this.calc()
                 break
 
             case 'dot':
